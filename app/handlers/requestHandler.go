0bfe050e904ccaa394f3f6ed6f2d5fa7055d213f
isif00 <sifodev@gmail.com>
2024-07-25 18:31:10 +0100

    codecrafters submit [skip ci]

--- app/handlers/requestHandler.go
+++ app/handlers/requestHandler.go
@@ -33,7 +33,7 @@ func RequestHandler(conn net.Conn) {
 	case path == "/":
 		response = utils.GetStatus(200, "OK\r\n")
 
-	case strings.HasPrefix(path, "/echo/"):
+	case path == "/echo":
 		parts := strings.SplitN(path, "/", 3)
 		if len(parts) < 3 {
 			conn.Write([]byte(utils.GetStatus(400, "Bad Request\r\n")))
